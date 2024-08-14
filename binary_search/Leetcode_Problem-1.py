97b2a4c38f144ccbc304dfc853b25cd5f114cc73
Deeksha Tiwari <deeksha9107@users.noreply.github.com>
2024-08-13 23:39:27 -0400

    Leetocde_Problem_1 Completed

--- binary_search/Leetcode_Problem-1.py
+++ binary_search/Leetcode_Problem-1.py
@@ -28,8 +28,54 @@
 
 # Step 4: Solution without code
     # Brute Force Approach: Since searching in a sorted array --> Linear Search --> O(n)
-    # 
+    # Binary Search will half the time --> will take 2 ptrs low and high and find if target in range
+
 # Step 5: Write out solution in code
+
+# // Your code here along with comments explaining your approach in three sentences only
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # checking if array is empty
+        if len(nums) == 0:
+            #not in nums
+            return -1
+        #taking two ptrs low
+        low = 0
+        #and high
+        high = len(nums) - 1
+        #while loop
+        while (low <= high):
+            #calculating mid 
+            mid = (low + high) // 2
+            #if mid has target
+            if (nums[mid] == target):
+                #in nums return mid
+                return mid
+            #check if left sorted
+            elif (nums[low] <= nums[mid]):
+                #left sorted
+                #check if target present in left part
+                if (target >= nums[low] and target < nums[mid]):
+                    # if present we shorten our window by 1/2
+                    high = mid - 1
+                else:
+                    # we move low to the other half
+                    low = mid + 1
+            else:
+                #right sorted
+                #check if target present in right part
+                if (target > nums[mid] and target <= nums[high]):
+                    # we move low to the other half
+                    low = mid + 1
+                else:
+                    # if present we shorten our window by 1/2
+                    high = mid - 1
+        return -1
+    
 # Step 6: Double check for errors
+
 # Step 7: Testing solution
+
 # Step 8: Space & Time Complexity
+# Time Complexity : O(logm + logn) = O(logmn)
+# Space Complexity : O(1)
